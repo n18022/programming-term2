5f3cb6509b400f2ffcff39cf1030efc84a64c256
n18022 <n18022@std.it-college.ac.jp>
2018-07-09 17:35:37 +0900

    task20180705.py

--- src/basic-c2/task20180705.py
+++ src/basic-c2/task20180705.py
@@ -1,14 +1,22 @@
 print("会計いくら？")
+#ビールの会計
 bi_runedan = 200
 bi_rukazu = 2
 bi_rukaikei = bi_runedan * bi_rukazu
+
+#おつまみの会計
 otumaminedan = 100
 otumamikazu = 1
 otumamikaikei = otumaminedan * otumamikazu
+
+#焼き鳥の会計
 yakitorinedan = 100
 yakitorikazu = 2
 waribikiritu = 1 - 0.2
 yakitorikaikei = (yakitorinedan * waribikiritu) * yakitorikazu
+
+#割引ポイント
 pointo = 150
+#すべての会計
 kaikei = bi_rukaikei + otumamikaikei + yakitorikaikei - pointo
 print(kaikei)
