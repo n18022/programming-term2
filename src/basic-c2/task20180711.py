4b5ae6751d15d99dfd772cff6b2b4963a072b69b
n18022 <n18022@std.it-college.ac.jp>
2018-07-11 16:43:49 +0900

    task20180711.py

---
+++ src/basic-c2/task20180711.py
@@ -0,0 +1,15 @@
+# 絶対に勝てないじゃんけん
+nyuuryoku = input("1,2,3どれかを入力してね\n何を出しますか? 1:グー、2:チョキ、3:パー\n")
+nyuuryoku = str(nyuuryoku)
+aite = ""
+if (nyuuryoku == "1"):
+    aite = "パー"
+    print("CPUが{}を出しました。あなたの負けです。".format(aite))
+elif (nyuuryoku == "2"):
+    aite = "グー"
+    print("CPUが{}を出しました。あなたの負けです。".format(aite))
+elif (nyuuryoku == "3"):
+    aite = "チョキ"
+    print("CPUが{}を出しました。あなたの負けです。".format(aite))
+else:
+    print("入力が不正です。終了します。")
