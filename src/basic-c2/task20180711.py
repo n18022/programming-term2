8d75ee248b326f3c0f7bde5248ff092adae4378a
n18022 <n18022@std.it-college.ac.jp>
2018-07-12 12:41:25 +0900

    task20180711.py

--- src/basic-c2/task20180711.py
+++ src/basic-c2/task20180711.py
@@ -1,14 +1,14 @@
 # 絶対に勝てないじゃんけん
 nyuuryoku = input("1,2,3どれかを入力してね\n何を出しますか? 1:グー、2:チョキ、3:パー\n")
+# 入力を文字型に変換する。
 nyuuryoku = str(nyuuryoku)
-aite = ""
-if (nyuuryoku == "1"):
+if nyuuryoku == "1":
     aite = "パー"
     print("CPUが{}を出しました。あなたの負けです。".format(aite))
-elif (nyuuryoku == "2"):
+elif nyuuryoku == "2":
     aite = "グー"
     print("CPUが{}を出しました。あなたの負けです。".format(aite))
-elif (nyuuryoku == "3"):
+elif nyuuryoku == "3":
     aite = "チョキ"
     print("CPUが{}を出しました。あなたの負けです。".format(aite))
 else:
