b35d0b903e3d8229a77d296504c384cb43438734
n18022 <n18022@std.it-college.ac.jp>
2018-07-17 10:26:55 +0900

    task20180712for.py

--- src/basic-c2/task20180712for.py
+++ src/basic-c2/task20180712for.py
@@ -1,6 +1,11 @@
 print("  |   1  2  3  4  5  6  7  8  9")
 print("--+----------------------------")
-for i in range(1):
-    a = 1
-    for i in range(1, 10):
+# kaisuuはループの回数を表す。
+kaisuu = 1
+for _ in range(kaisuu):
+    # kaisiはiループの開始の値
+    kaisi = 1
+    # owariはiループの終わりの値
+    owari = 10
+    for i in range(kaisi, owari):
         print("{:>2}|  {:>2} {:>2} {:>2} {:>2} {:>2} {:>2} {:>2} {:>2} {:>2}".format(i, i, 2*i, 3*i, 4*i, 5*i, 6*i, 7*i, 8*i, 9*i))
