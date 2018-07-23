8e610a8b2b4954a3fd357c9135a1856177e13a46
n18022 <n18022@std.it-college.ac.jp>
2018-07-23 17:01:23 +0900

    task20180719.py

--- src/basic-c3/task20180719.py
+++ src/basic-c3/task20180719.py
@@ -1,8 +1,8 @@
-def _3_(parameter1, parameter2, Calculation_method):
+def _3_(parameter1, parameter2, calculation_method):
     """
     ２つの値と演算方法を入力して計算する。
 
-    Parameters
+    parameters
     ----------
     parameter1 : float
       第1入力パラメータ入力
@@ -10,7 +10,7 @@ def _3_(parameter1, parameter2, Calculation_method):
     parameter2 : float
       第2入力パラメータ入力
 
-    Calculation_method
+    calculation_method
       演算方法を入力
 
     Returns
@@ -21,12 +21,12 @@ true = True
 # 第1と第2のパラメータに数字が入るまで繰り返す。
 while True:
     parameter1 = input("第1パラメータを入力してください\n")
-    P1True = str.isnumeric(parameter1)
-    if P1True != true:
+    p1True = str.isnumeric(parameter1)
+    if p1True != true:
         continue
     parameter2 = input("第2パラメータを入力してください\n")
-    P2True = str.isnumeric(parameter2)
-    if P2True != true:
+    p2True = str.isnumeric(parameter2)
+    if p2True != true:
         continue
     parameter1 = float(parameter1)
     parameter2 = float(parameter2)
@@ -35,20 +35,20 @@ while True:
 # 四則演算のどれかを入力するまで繰り返す
 # 入力後、第1と第2のパラメータを使って計算する。
 while True:
-    Calculation_method = input("演算方法を入力してください\n")
-    if Calculation_method == "+":
+    calculation_method = input("演算方法を入力してください\n")
+    if calculation_method == "+":
         add = parameter1 + parameter2
         print(add)
         break
-    if Calculation_method == "-":
+    if calculation_method == "-":
         sub = parameter1 - parameter2
         print(sub)
         break
-    if Calculation_method == "*":
+    if calculation_method == "*":
         mul = parameter1 * parameter2
         print(mul)
         break
-    if Calculation_method == "/":
+    if calculation_method == "/":
         div = round((parameter1 / parameter2), 2)
         print(div)
         break
