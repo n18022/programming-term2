b4feb87257461b1ab2e39b02f7adab37fdebab5a
n18022 <n18022@std.it-college.ac.jp>
2018-07-23 17:00:21 +0900

    task20180718.py

--- src/basic-c3/task20180718.py
+++ src/basic-c3/task20180718.py
@@ -1,28 +1,79 @@
+# 定義達
+order_content = []
+sum_price = []
+decision = ""
+end = 1
+
 # メニューを見せる
-mainmenue = {"DripCoffee": 280, "ColdBrewCoffee": 320, "CafeLatte": 330, "SoyLatte": 380, "Cappuccino": 330, "CaramelFrappuccino": 470, "MacchaCreamFrappuccino": 470}
+mainmenues = {
+    "DripCoffee": 280,
+    "ColdBrewCoffee": 320,
+    "CafeLatte": 330,
+    "SoyLatte": 380,
+    "Cappuccino": 330,
+    "CaramelFrappuccino": 470,
+    "MacchaCreamFrappuccino": 470
+    }
+
 # オプションを見せる
-option = {"LowFatMilk": 0, "NonFatMilk": 0, "SoyMilk": 50, "DeepCoffee": 60}
-# 注文内容
-Order_content = []
+option = {
+    "LowFatMilk": 0,
+    "NonFatMilk": 0,
+    "SoyMilk": 50,
+    "DeepCoffee": 60,
+    "WhipCream": 70,
+    "CaramelShrup": 60,
+    "ChocoSource": 0,
+    "DeDafe": 50
+    }
+
+# メインメニュー注文
 while True:
-    menue = input("メインメニューを選んでください。")
-    if menue == "" or menue == "q":
+    get_main_menue = input("メインメニューを選んでください。\n")
+    # 入力なしかqの場合のみ購入をキャンセルする。
+    if get_main_menue == "" or get_main_menue == "q":
         print("購入をキャンセルします。")
-        End = 1
         break
-    if menue in mainmenue:
+    # 入力したオプションメニューが正しければ実行
+    if get_main_menue in mainmenues:
         print("メインメニューを承りました。")
-        Order_content.append(menue)
-        End = 0
+        order_content.append(get_main_menue)
+        sum_price = mainmenues[get_main_menue]
+        end = 0
         break
-    print("選択されたメニューはありません。")
-while End != 1:
-    is_option_ordering = input("オプションメニューを選んでください。")
-    if is_option_ordering == "" or is_option_ordering == "q":
-        print("注文内容は{}{}です。".format(menue, ))
+    print("選択されたメニューはありません。\n")
+
+# メインメニューが注文されていたらオプションメニュー注文
+while end != 1:
+    # 最初だけ
+    if decision != "１回目以降":
+        get_option_menue = input("オプションメニューを選んでください。\n")
+    # １回目以降以降
+    if decision == "１回目以降":
+        get_option_menue = input("他にオプションメニューの注文はございますか？\n")
+    # ""か"p"だったら注文終了
+    if get_option_menue == "" or get_option_menue == "q":
         break
-    if is_option_ordering in option:
-        print("オプションを承りました。")
-        Order_content.append(is_option_ordering)
+    # オプションメニュー正しければ実行するよ。
+    while get_option_menue in option:
+        # name, priceにそれぞれキー, 値を入力してるよ。
+        for name, price in option.items():
+            # どのオプションメニューを求めてるのか調べてべてるよ。
+            if name == get_option_menue:
+                decision = "オプションを承りました。"
+                print("オプションを承りました。")
+                order_content.append(get_option_menue)
+                sum_price += price
+                break
+        if decision == "オプションを承りました。":
+            break
+    if decision == "オプションを承りました。":
+        decision = "１回目以降"
         continue
-    print("選択されたメニューはありません。")
+    if decision != "オプションを承りました。":
+        print("選択されたメニューはありません。")
+
+# メインメニューが注文されていたら実行するよ。
+if end != 1:
+    print("注文内容は{}です。".format(order_content))
+    print("合計金額は{}円です。右奥のカウンターにてお待ちください。".format(sum_price))
